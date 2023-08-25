2ee40b68bf9d527ca2460246f5d176238004f550
Henry Kinoshita <42702870+PEROLOKO@users.noreply.github.com>
2023-08-25 15:24:25 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ScryFallManager-CP4-EAD
+ Gerenciador para o site https://scryfall.com (não oficial)
---
+++ ScryFallManager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ScryFallManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
