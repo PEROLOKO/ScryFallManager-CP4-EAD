54a821d1e89109d345058247479e69aa24f419b0
Henry Kinoshita <42702870+PEROLOKO@users.noreply.github.com>
2023-08-31 16:50:54 -0300

    arrumado readme e validation

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ScryFallManager-CP4-EAD
  Gerenciador para o site https://scryfall.com (não oficial)
+
+## Integrantes
+ - Enzo Perazolo RM95657
+ - Giovanna Sousa RM94767
+ - Henry Kinoshita RM93443
+ - Luiz Felipe Souza de Oliveira RM94538
+ - Matheus Felipe RM93772
--- ScryFallManager/Validation/CartaValidation.cs
+++ ScryFallManager/Validation/CartaValidation.cs
@@ -6,7 +6,10 @@ namespace ScryFallManager.Validation
     public class CartaValidation : AbstractValidator<Carta>
     {
         public CartaValidation() {
-            
+            RuleFor(x => x.Nome).NotEmpty().NotNull();
+            RuleFor(x => x.Texto).NotEmpty().NotNull();
+            RuleFor(x => x.Raridade).NotNull();
+            RuleFor(x => x.DataLancamento).NotNull();
         }
     }
 }
